6b676236b48e282c6668e07ce833a7562e52aaa2
35917560 <zw19860913@gmail.com>
2017-09-07 01:01:09 +0800

    CreditContract

--- contracts/PledgeManager.sol
+++ contracts/PledgeManager.sol
@@ -1,23 +1,28 @@
 pragma solidity ^0.4.13;
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
-contract PledgeManager is Owner{
+contract PledgeManager is Ownable {
 	struct pledge{
 		string symbol;
 		address tokenAddress;
 		string anchoringRestApi;
 	}
 
-	mapping(sting=>pledge) pledges;
+	mapping(string=>pledge) pledges;
 
-	function addPledge(string _symbol,address tokenAddress,string anchoringRestApi) external
-		ownerOnly
+	function addPledge(string _symbol,address _tokenAddress,string _anchoringRestApi) external
+		onlyOwner
 	{
 		pledge storage p = pledges[_symbol];
-		if(p.symbol!=null){
-
-		}else{
-
-		}
-
+		p.symbol=_symbol;
+		p.tokenAddress=_tokenAddress;
+		p.anchoringRestApi=_anchoringRestApi;
 	}
+
+    function getPledge(string _symbol) public returns (string,address,string)
+
+    {
+        pledge storage p=pledges[_symbol];
+
+        return (p.symbol,p.tokenAddress,p.anchoringRestApi);
+    }
 }
